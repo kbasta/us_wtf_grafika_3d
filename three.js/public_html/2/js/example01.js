f1a75175c25dddd1626ef67a310ca690e2c5e95f
kbasta <k_basta@hotmail.com>
2018-04-09 12:04:40 +0200

    Dodanie kwadratu + animacja przesuwania w jedną stronę

--- three.js/public_html/2/js/example01.js
+++ three.js/public_html/2/js/example01.js
@@ -1,42 +1,30 @@
-function init(){
-    camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
-    camera.position.z = 5;
-    
-    var v1 = new THREE.Vector3(0, 2, 0);
-    var v2 = new THREE.Vector3(-2, 0, 0);
-    var v3 = new THREE.Vector3(2, -2, 0);
-    var v4 = new THREE.Vector3(-2, 2, 0);
+var tx = -4.5;
+var speed = 0.05;
 
-    var face = new THREE.Face3(0, 1, 3);
-    var face1 = new THREE.Face3(0, 2, 4);
-    
+function createSquare(){
     var geom = new THREE.Geometry();
-    geom.vertices.push(v4);
-    geom.vertices.push(v2);
-    geom.vertices.push(v3);
-    geom.faces.push(face);
     
-    var geom1 = new THREE.Geometry();
-    geom1.vertices.push(v1);
-    geom1.vertices.push(v2);
-    geom1.vertices.push(v4);
-    geom1.faces.push(face1);
+    geom.vertices.push(new THREE.Vector3(0.5, 0.5, 0));
+    geom.vertices.push(new THREE.Vector3(-0.5, 0.5, 0));
+    geom.vertices.push(new THREE.Vector3(-0.5, -0.5, 0));
+    geom.vertices.push(new THREE.Vector3(0.5, -0.5, 0));
+
+    geom.faces.push(new THREE.Face3(0,1,2));
+    geom.faces.push(new THREE.Face3(0,2,3));
     
-    geom.faces[0].vertexColors[0] = new THREE.Color(0xFF0000);
-    geom.faces[0].vertexColors[1] = new THREE.Color(0xFF00FF);
-    geom.faces[0].vertexColors[2] = new THREE.Color(0x0000FF);
+    return geom;
+}
+
+function init(){
+    var aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspectiveCamera(45, aspect, 1, 1000);
+    camera.position.z = 8;
     
-    geom1.faces[0].vertexColors[0] = new THREE.Color(0xFF00FF);
-    geom1.faces[0].vertexColors[1] = new THREE.Color(0xFF0000);
-    geom1.faces[0].vertexColors[2] = new THREE.Color(0x0000FF);
-    
-    var material = new THREE.MeshBasicMaterial({vertexColors:THREE.VertexColors});
-    var triangle = new THREE.Mesh(geom, material);
-    var triangle1 = new THREE.Mesh(geom1, material);
+    var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    square = new THREE.Mesh(createSquare(), material);
     
     scene = new THREE.Scene();
-    scene.add(triangle);
-    scene.add(triangle1);
+    scene.add(square);
     
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -47,12 +35,20 @@ function init(){
 }
 
 function onWindowResize() {
-    //camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function animate() {
+    requestAnimationFrame(animate);
+    square.position.x = tx;
+    tx += speed;
+    renderer.render(scene, camera);
+    
+}
+
 function main(){
     requestAnimationFrame(main);
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 }

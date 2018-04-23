c952e718dcbe1ae134b5c9f63c7abd1fdb10c574
kbasta <k_basta@hotmail.com>
2018-04-23 11:59:58 +0200

    Dodanie animowanego, cieniowanego, fioletowego pierścienia

--- three.js/public_html/4/js/example01.js
+++ three.js/public_html/4/js/example01.js
@@ -1,28 +1,18 @@
-// sposoby reprezentacji obiektów 3d
+// cieniowanie flat
 var scene, camera, renderer;
 
 function init(){
     var aspect = window.innerWidth / window.innerHeight;
     camera = new THREE.PerspectiveCamera(70, aspect, 1, 10000);
-    camera.position.z = 400;
+    camera.position.z = 15;
     scene = new THREE.Scene();
     
-    var geometry1 = new THREE.SphereGeometry(120, 10, 10);
-    geometry1.translate(0,0,250);
-    mesh1 = new THREE.Mesh(geometry1, setTexture('earth.jpg'));
+    mesh = new THREE.Mesh(new THREE.TorusGeometry(5 , 3, 16, 30), new THREE.MeshPhongMaterial({shading: THREE.FlatShading}));
+    scene.add(mesh);
     
-    var geometry2 = new THREE.SphereGeometry(80, 10, 10);
-    geometry2.translate(150,0,0);
-    mesh2 = new THREE.Mesh(geometry2, setTexture('neptune.jpg'));
-   
-    var geometry3 = new THREE.SphereGeometry(30, 10, 10);
-    geometry3.translate(0,10,10);
-    mesh3 = new THREE.Mesh(geometry3, setTexture('sun.jpg'));
-    //mesh3 = new THREE.Mesh(geometry3, new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe: true}));
-    
-    scene.add(mesh1);
-    scene.add(mesh2);
-    scene.add(mesh3);
+    var light = new THREE.PointLight(0xff00ff, 1, 500);
+    light.position.set(10, 10, 50);
+    scene.add(light);
     
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -46,12 +36,9 @@ function setTexture(path){
 }
 
 function rotateMeshes(){
-    mesh1.rotation.y += 0.02;
-    mesh2.rotation.x -= 0.1;
-    mesh2.rotation.y -= 0.1;
-    mesh2.rotation.z += 0.02;
-    mesh3.rotation.x += 0.04; 
-    mesh3.rotation.y += 0.02;
+    mesh.rotation.y += 0.02;
+    mesh.rotation.z += 0.02;
+    mesh.rotation.x += 0.5;
 }
 
 function animate() {
